e63f08a5c5099ea7a4dffa13047b5ee55ead0aab
Cameron MacFarland <distantcam@gmail.com>
2022-12-28 15:19:58 +0800

    Added tests

---
+++ src/FediNet.IntegrationTests/BasicTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoCtor;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace FediNet.IntegrationTests;
+
+[AutoConstruct]
+[UsesVerify]
+public partial class BasicTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    [Theory]
+    [InlineData("/.well-known/nodeinfo")]
+    [InlineData("/nodeinfo/2.0.json")]
+    public async Task VerifyResponses(string url)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        // Assert
+        await Verify(response).UseParameters(url.Replace('/', '_'));
+    }
+}
